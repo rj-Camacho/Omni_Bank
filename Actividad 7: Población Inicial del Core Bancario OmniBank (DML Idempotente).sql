9cb33b209a4e3e8dbbfa79eea0e9be8d5a0db487
Bob <roberjvc@gmail.com>
2026-09-18 04:06:12 -0600

    Actividad 7: Población Inicial del Core Bancario OmniBank (DML Idempotente).sql
    Aprendiste a evitar errores de duplicados con ON CONFLICT y a obtener confirmación inmediata del saldo al instante usando RETURNING.

--- Actividad 7: Población Inicial del Core Bancario OmniBank (DML Idempotente).sql
+++ Actividad 7: Población Inicial del Core Bancario OmniBank (DML Idempotente).sql
@@ -1,6 +1,5 @@
 BEGIN;
 
-
 INSERT INTO core.customers (
     customer_id, first_name, last_name, email, phone, tax_id, date_of_birth
 ) VALUES 
@@ -8,7 +7,6 @@ INSERT INTO core.customers (
     ('22222222-2222-2222-2222-222222222222', 'Jane', 'Smith', 'jane.smith@email.com', '555-0200', 'TAX-200', '1990-08-25'),
     ('33333333-3333-3333-3333-333333333333', 'Alice', 'Johnson', 'alice.j@email.com', '555-0300', 'TAX-300', '1978-11-05')
 ON CONFLICT (email) DO NOTHING;
-
 
 INSERT INTO core.accounts (
     account_id, customer_id, account_number, account_type, balance, credit_limit
@@ -18,8 +16,6 @@ INSERT INTO core.accounts (
     ('cccccccc-cccc-cccc-cccc-cccccccccccc', '33333333-3333-3333-3333-333333333333', 'ACCT-3001', 'CREDIT', 0.00, 10000.00)
 ON CONFLICT (account_number) DO NOTHING;
 
-
-
 INSERT INTO core.transactions (
     transaction_id, from_account_id, to_account_id, amount, transaction_type, status, description
 ) VALUES 
@@ -27,7 +23,6 @@ INSERT INTO core.transactions (
     ('20000000-0000-0000-0000-000000000002', 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 'cccccccc-cccc-cccc-cccc-cccccccccccc', 300.00, 'TRANSFER', 'COMPLETED', 'Abono a tarjeta de crédito de Alice'),
     ('30000000-0000-0000-0000-000000000003', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', NULL, 50.00, 'FEE', 'COMPLETED', 'Cobro por comisión mensual bancaria')
 ON CONFLICT (transaction_id) DO NOTHING;
-
 
 UPDATE core.accounts 
 SET balance = balance - 50.00 
